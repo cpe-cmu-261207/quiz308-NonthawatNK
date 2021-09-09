cd902e1ecd915393d5982438da5361bfd798e677
NonthawatNK <Nonthawt20@gmail.com>
2021-09-09 14:39:36 +0700

    :cry: Sorry for my self :cry:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useState } from 'react';
+import { comments } from './data/comments';
 import './App.css';
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
 
           {/* image and name */}
           <div className="flex space-x-2 items-center">
-            <img className="w-12 h-12 rounded-full" src="/profileImages/handsome.jpg"></img>
-            <span className='font-semibold text-lg text-white'>Chayanin Suatap 610631100</span>
+            <img className="w-12 h-12 rounded-full" src="/profileImages/Big_G.png"></img>
+            <span className='font-semibold text-lg text-white'>Nonthawat Kongsichai 610610547</span>
           </div>
 
           {/* status message */}
@@ -33,14 +35,14 @@ function App() {
 
           {/* normal comment */}
           <div className="flex p-2 items-start space-x-2">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/lisa.jpg"></img>
+            <img className="w-10 w-10 rounded-full" src={comments[0].userImagePath}></img>
             <div className="bg-gray-500 rounded-lg p-2">
-              <p className="font-semibold text-white">Lisa</p>
-              <p className='text-white'>ตัวอย่าง Template จ้า</p>
+              <p className="font-semibold text-white">{comments[0].username}</p>
+              <p className='text-white'>{comments[0].commentText}</p>
               {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
               <div className='flex items-center'>
                 <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-300'>999 คน</p>
+                <p className='text-gray-300'>{comments[0].likeNum}</p>
               </div>
             </div>
           </div>
@@ -48,10 +50,41 @@ function App() {
           {/* replies */}
           {/* ต่างกันตรงที่มี padding มากกว่าเท่านั้น (pl-14) */}
           <div className="flex p-2 items-start space-x-2 pl-14">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/puppy.jpg"></img>
+            <img className="w-10 w-10 rounded-full" src={comments[0].replies[0].userImagePath}></img>
             <div className="bg-gray-500 rounded-lg p-2">
-              <p className="font-semibold text-white">หมาน้อย</p>
-              <p className='text-white'>เม้นค้าบ</p>
+              <p className="font-semibold text-white">{comments[0].replies[0].username}</p>
+              <p className='text-white'>{comments[0].replies[0].commentText}</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>{comments[0].replies[0].likeNum}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* {repliee2} */}
+          <div className="flex p-2 items-start space-x-2 pl-14">
+            <img className="w-10 w-10 rounded-full" src={comments[0].replies[1].userImagePath}></img>
+            <div className="bg-gray-500 rounded-lg p-2">
+              <p className="font-semibold text-white">{comments[0].replies[1].username}</p>
+              <p className='text-white'>{comments[0].replies[1].commentText}</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>{comments[0].replies[1].likeNum}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Normalcomment */}
+          <div className="flex p-2 items-start space-x-2">
+            <img className="w-10 w-10 rounded-full" src={comments[1].userImagePath}></img>
+            <div className="bg-gray-500 rounded-lg p-2">
+              <p className="font-semibold text-white">{comments[1].username}</p>
+              <p className='text-white'>{comments[1].commentText}</p>
+              {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>{comments[1].likeNum}</p>
+              </div>
             </div>
           </div>
 
